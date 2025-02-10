3aa5a3c48c644257f13d4e698a948a5537525a7d
Gianlu201 <gianlucadidiego2001@gmail.com>
2025-02-10 15:40:07 +0100

    Athlete class implemented

--- FS0924_S13-L1/Athlete.cs
+++ FS0924_S13-L1/Athlete.cs
@@ -8,6 +8,73 @@ namespace FS0924_S13_L1
 {
     public class Athlete
     {
-        
+        private string? _firstName;
+        private string? _lastName;
+        private int _age;
+        private string? _sport;
+        private int _competitionsWinned;
+
+        public string FirstName
+        {
+            get{
+                return _firstName;
+            }
+            set{
+                _firstName = value;
+            }
+        }
+
+        public string LastName
+        {
+            get
+            {
+                return _lastName;
+            }
+            set
+            {
+                _lastName = value;
+            }
+        }
+
+        public int Age
+        {
+            get
+            {
+                return _age;
+            }
+            set
+            {
+                _age = value;
+            }
+        }
+
+        public string Sport
+        {
+            get
+            {
+                return _sport;
+            }
+            set
+            {
+                _sport = value;
+            }
+        }
+
+        public int CompetitionsWinned
+        {
+            get
+            {
+                return _competitionsWinned;
+            }
+            set
+            {
+                _competitionsWinned = value;
+            }
+        }
+
+        public string Presentation()
+        {
+            return "My name is " + this.FirstName + " " + this.LastName + ", I'm " + this.Age + " years old, and I'm an athlete. I do " + this.Sport + " and I won " + this.CompetitionsWinned + " championships";
+        }
     }
 }
--- FS0924_S13-L1/Program.cs
+++ FS0924_S13-L1/Program.cs
@@ -1,1 +1,10 @@
-﻿Console.WriteLine("Hello World!");
+﻿using FS0924_S13_L1;
+
+var athlete1 = new Athlete();
+athlete1.FirstName = "Simone";
+athlete1.LastName = "Giannelli";
+athlete1.Age = 20;
+athlete1.Sport = "volleyball";
+athlete1.CompetitionsWinned = 3;
+
+Console.WriteLine(athlete1.Presentation());
